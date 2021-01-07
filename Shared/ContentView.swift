73de7f6db0743aabeafbfd235f697abba5514fd9
Michele Volpato <mvolpato@users.noreply.github.com>
2021-01-07 18:10:31 +0100

    Using size classes with AnyView type erasure

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,25 +8,6 @@
 import SwiftUI
 import CoreData
 
-struct PushButton: View {
-    let title: String
-    @Binding var isOn: Bool
-
-    var onColors = [Color.red, Color.yellow]
-    var offColors = [Color(white: 0.6), Color(white: 0.4)]
-
-    var body: some View {
-        Button(title) {
-            self.isOn.toggle()
-        }
-        .padding()
-        .background(LinearGradient(gradient: Gradient(colors: isOn ? onColors : offColors), startPoint: .top, endPoint: .bottom))
-        .foregroundColor(.white)
-        .clipShape(Capsule())
-        .shadow(radius: isOn ? 0 : 5)
-    }
-}
-
 struct ContentView: View {
 //    @Environment(\.managedObjectContext) private var viewContext
 //
@@ -35,14 +16,23 @@ struct ContentView: View {
 //        animation: .default)
 //    private var items: FetchedResults<Item>
 
-    @State private var rememberMe = false
+    @Environment(\.horizontalSizeClass) var sizeClass
 
-        var body: some View {
-            VStack {
-                PushButton(title: "Remember Me", isOn: $rememberMe)
-                Text(rememberMe ? "On" : "Off")
+    var body: some View {
+        if sizeClass == .compact {
+            return AnyView(VStack {
+                Text("Active size class:")
+                Text("COMPACT")
             }
+            .font(.largeTitle))
+        } else {
+            return AnyView(HStack {
+                Text("Active size class:")
+                Text("REGULAR")
+            }
+            .font(.largeTitle))
         }
+    }
 
 //    private func addItem() {
 //        withAnimation {
