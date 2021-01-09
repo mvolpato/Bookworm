c9babc287c15762efedb8ea9a8fc68289f7bc825
Michele Volpato <mvolpato@users.noreply.github.com>
2021-01-09 22:03:54 +0100

    Create initial UI

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,63 +9,36 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(entity: Book.entity(), sortDescriptors: []) var books: FetchedResults<Book>
     
-    //    @FetchRequest(
-    //        sortDescriptors: [NSSortDescriptor(keyPath: \Student.id, ascending: true)],
-    //        animation: .default)
-    @FetchRequest(entity: Student.entity(), sortDescriptors: []) var students: FetchedResults<Student>
-    //    private var items: FetchedResults<Item>
+    @State private var showingAddScreen = false
     
     var body: some View {
-        VStack {
-            Text("Students:")
+        NavigationView {
             List {
-                ForEach(students, id: \.id) { student in
-                    Text(student.name ?? "Unknown")
+                ForEach(books, id: \.self) { book in
+                    NavigationLink(destination: Text(book.title ?? "Unknown Title")) {
+                        EmojiRatingView(rating: book.rating)
+                            .font(.largeTitle)
+                        
+                        VStack(alignment: .leading) {
+                            Text(book.title ?? "Unknown Title")
+                                .font(.headline)
+                            Text(book.author ?? "Unknown Author")
+                                .foregroundColor(.secondary)
+                        }
+                    }
                 }
-                .onDelete(perform: deleteStudent)
             }
-            Button(action: addStudent) {
-                Label("Add student", systemImage: "plus")
-            }
-        }
-    }
-    
-    private func addStudent() {
-        let firstNames = ["Ginny", "Harry", "Hermione", "Luna", "Ron"]
-        let lastNames = ["Granger", "Lovegood", "Potter", "Weasley"]
-
-        let chosenFirstName = firstNames.randomElement()!
-        let chosenLastName = lastNames.randomElement()!
-
-        let student = Student(context: viewContext)
-        student.id = UUID()
-        student.name = "\(chosenFirstName) \(chosenLastName)"
-        
-        withAnimation {
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-    
-    private func deleteStudent(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { students[$0] }.forEach(viewContext.delete)
-            
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            .navigationBarTitle("Bookworm")
+            .navigationBarItems(trailing: Button(action: {
+                self.showingAddScreen.toggle()
+            }) {
+                Image(systemName: "plus")
+            })
+            .sheet(isPresented: $showingAddScreen) {
+                AddBookView().environment(\.managedObjectContext, self.moc)
             }
         }
     }
@@ -73,6 +46,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView()
     }
 }
--- Shared/Persistence.swift
+++ Shared/Persistence.swift
@@ -2,32 +2,13 @@
 //  Shared
 //
 //  Created by Michele Volpato on 07/01/2021.
-//  
+//
 //
 
 import CoreData
 
 struct PersistenceController {
     static let shared = PersistenceController()
-
-    static var preview: PersistenceController = {
-        let result = PersistenceController(inMemory: true)
-        let viewContext = result.container.viewContext
-        for _ in 0..<10 {
-            let newStudent = Student(context: viewContext)
-            newStudent.name = "name"
-            newStudent.id = UUID()
-        }
-        do {
-            try viewContext.save()
-        } catch {
-            // Replace this implementation with code to handle the error appropriately.
-            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            let nsError = error as NSError
-            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-        }
-        return result
-    }()
 
     let container: NSPersistentContainer
 
